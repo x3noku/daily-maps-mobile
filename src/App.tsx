085a4e3b78d21b90375c2717de2522054639c5e8
Dmitry <labamnus@gmail.com>
2021-02-19 16:37:53 +0600

    Linters and remove .idea files commit
    - Remove .idea directory
    - Install and setup linters

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
-import {
-  View,
-  Text,
-} from 'react-native';
+import { View, Text } from 'react-native';
 
 const App = () => {
-  return (
-      <>
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-          <Text>Daily Maps Mobile</Text>
-        </View>
-      </>
-  );
+    return (
+        <>
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <Text>Daily Maps Mobile</Text>
+            </View>
+        </>
+    );
 };
 
 export default App;
